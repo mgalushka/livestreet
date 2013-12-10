912feb73162743ec5fe1f96a8573a4f249e28aab
Maxim Galushka <mgalushka@gmail.com>
2013-12-10 15:50:15 +0200

    premoderation

---
+++ plugins/premoderation/templates/language/ukrainian.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+	// Шаблон
+	'premoderation_main_menu_title'			=> 'Публікації на модерації',
+	'premoderation_aproove'					=> 'Затвердити',
+	'premoderation_disaproove'				=> 'Відхилити',
+	'premoderation_reason'					=> 'Причина',
+	
+	// Нотисы
+	'premoderation_wait_until_premoderate'	=> 'Дякуємо! Ваша публікація з\'виться після перевірки модератором',
+	
+	'premoderation_notice_title'			=> 'Нова публікація на модерації',
+	'premoderation_notice_body'				=> 'На модерацяю надійшла нова публікація. Будь ласка, ознайомтесь: ',
+	
+	'premoderation_notice_topicapproved'	=> 'Вашу публікацію схвалено',
+	'premoderation_notice_topicadispproved'	=> 'Ваша публікация не пройшла модерацію. Причина:',
+	
+);
+
+?>
